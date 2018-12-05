ceb4f2b353dc655d1c2b902eeaec2fa9533def65
dodiws <dodiws@gmail.com>
2018-12-05 11:21:01 +0700

    up

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='isdc-flood',
-    version='0.1dev1',
+    version='0.1dev2',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # temporary license
